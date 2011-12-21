76f7d2a3b5744a4f5ae404fbdfeaa0c70c5ae30a
Diederik van der Boor <vdboor@edoburu.nl>
2011-12-21 13:31:44 +0100

    Place add_introspection_rules() outside try code.

--- form_designer/__init__.py
+++ form_designer/__init__.py
@@ -1,8 +1,9 @@
 try:
     from south.modelsinspector import add_introspection_rules
+except ImportError:
+    pass
+else:
     add_introspection_rules([], ['^form_designer\.pickled_object_field\.PickledObjectField'])
     add_introspection_rules([], ['^form_designer\.model_name_field\.ModelNameField'])
     add_introspection_rules([], ['^form_designer\.template_field\.TemplateCharField'])
     add_introspection_rules([], ['^form_designer\.template_field\.TemplateTextField'])
-except ImportError:
-    pass
