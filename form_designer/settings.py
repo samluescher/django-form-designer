1ebf0e100e56b3217492167bcf8a76a806d3ce96
Samuel Luescher <samuel@formlabs.com>
2014-04-11 14:32:15 -0400

    Use PickleField by default

--- form_designer/settings.py
+++ form_designer/settings.py
@@ -88,4 +88,6 @@ ALLOWED_FILE_TYPES = getattr(settings, 'FORM_DESIGNER_ALLOWED_FILE_TYPES', (
 MAX_UPLOAD_SIZE = getattr(settings, 'MAX_UPLOAD_SIZE', 5242880) # 5M
 MAX_UPLOAD_TOTAL_SIZE = getattr(settings, 'MAX_UPLOAD_TOTAL_SIZE', 10485760) # 10M
 
-VALUE_PICKLEFIELD = getattr(settings, 'FORM_DESIGNER_VALUE_PICKLEFIELD', False)
+# If true, submitted values won't be stored as strings, but serialized to a PickleField,
+# preserving the original type.
+VALUE_PICKLEFIELD = getattr(settings, 'FORM_DESIGNER_VALUE_PICKLEFIELD', True)
